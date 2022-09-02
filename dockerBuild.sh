c4a345b5e1f8197cdb546a99ab97c316772dc0a4
shawnhwl <110517211+shawnhwl@users.noreply.github.com>
2022-09-02 22:48:27 +0800

    Update dockerBuild.sh

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,4 +1,4 @@
 COMMIT=$(git rev-parse --verify HEAD)
 docker image build . -t "vidconf-ion-app-web_web:latest" -t "vidconf-ion-app-web_web:${COMMIT}"
-docker image tag vidconf-ion-app-web_web:latest howeli/vidconf-ion-app-web_web:latest
-docker push howeli/vidconf-ion-app-web_web:latest
+docker image tag vidconf-ion-app-web_web:latest howeli/vidconf-ion-app-web:latest
+docker push howeli/vidconf-ion-app-web:latest
