c40784dae4d0567e51020553b2581136d82ddefd
Shawn Ho <shawnhwl@yahoo.com.sg>
2022-11-08 23:40:02 +0800

    version tag docker images

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,4 +1,18 @@
+TAGNAME=$1
+
+show_help()
+{
+    echo ""
+    echo "Usage: ./dockerBuild.sh tagname"
+    echo ""
+}
+
+if [[ $# -ne 1 ]] ; then
+    show_help
+    exit 1
+fi
+
 COMMIT=$(git rev-parse --verify HEAD)
 docker image build . -t "vidconf-ion-app-web_web:latest" -t "vidconf-ion-app-web_web:${COMMIT}"
-docker image tag vidconf-ion-app-web_web:latest howeli/vidconf-ion-app-web:latest
-docker push howeli/vidconf-ion-app-web:latest
+docker image tag vidconf-ion-app-web_web:latest howeli/vidconf-ion-app-web:$TAGNAME
+docker push howeli/vidconf-ion-app-web:$TAGNAME
