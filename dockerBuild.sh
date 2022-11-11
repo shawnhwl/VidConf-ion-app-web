5745218d1fcfca7aacb643c00845532ab8ea3934
Shawn Ho <shawnhwl@yahoo.com.sg>
2022-11-11 17:43:15 +0800

    Initial Tag

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,18 +1,4 @@
-TAGNAME=$1
-
-show_help()
-{
-    echo ""
-    echo "Usage: ./dockerBuild.sh tagname"
-    echo ""
-}
-
-if [[ $# -ne 1 ]] ; then
-    show_help
-    exit 1
-fi
-
-COMMIT=$(git rev-parse --verify HEAD)
-docker image build . -t "vidconf-ion-app-web_web:latest" -t "vidconf-ion-app-web_web:${COMMIT}"
-docker image tag vidconf-ion-app-web_web:latest howeli/vidconf-ion-app-web:$TAGNAME
-docker push howeli/vidconf-ion-app-web:$TAGNAME
+TAGNAME=$(git describe)
+docker image build . -t "vidconf-ion-app-web_web:latest" -t "vidconf-ion-app-web_web:${TAGNAME}"
+docker image tag vidconf-ion-app-web_web:latest howeli/vidconf-ion-app-web:${TAGNAME}
+docker push howeli/vidconf-ion-app-web:${TAGNAME}
